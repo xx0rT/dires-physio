f78b40bd430f310ccd682074190d53d38c696e03
Troxx <121587043+xx0rT@users.noreply.github.com>
2026-01-01 18:22:09 +0100

    Updated features.tsx

--- src/components/layout/sections/features.tsx
+++ src/components/layout/sections/features.tsx
@@ -66,7 +66,7 @@ export const FeaturesSection = () => {
                     <span>Proč Si Vybrat</span>
                   <RotatingText
                     texts={['Certifikované Vzdělávání', 'Praktickou Výuku', 'Online Materiály', 'Zkušené Lektory', 'Malé Skupiny', 'Celoživotní Podporu']}
-                    mainClassName="px-2 sm:px-2 md:px-3 bg-cyan-300 text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
+                    mainClassName="px-2 sm:px-2 md:px-3 bg-purple-300 text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
                     staggerFrom={"last"}
                     initial={{ y: "100%" }}
                     animate={{ y: 0 }}
