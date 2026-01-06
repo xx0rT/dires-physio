778bba7c6880b5d508d8850ca2bc75357c6a647b
Troxx <121587043+xx0rT@users.noreply.github.com>
2026-01-06 21:55:59 +0100

    Updated marketing-layout.tsx

--- src/layouts/marketing-layout.tsx
+++ src/layouts/marketing-layout.tsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
 import { Navbar } from '@/components/layout/navbar'
 import { FooterSection } from '@/components/layout/sections/footer'
+import { PatternPlaceholder } from '@/components/shadcnblocks/pattern-placeholder'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 
@@ -16,12 +17,22 @@ export default function MarketingLayout() {
   }, [])
 
   return (
-    <div className="flex min-h-screen flex-col">
-      <Navbar />
-      <main className="flex-1">
-        <Outlet />
-      </main>
-      <FooterSection />
+    <div className="relative flex min-h-screen flex-col">
+      <PatternPlaceholder />
+      <div
+        className="absolute inset-0 z-0"
+        style={{
+          background:
+            "radial-gradient(125% 125% at 50% 90%, var(--background) 40%, var(--primary) 100%)",
+        }}
+      />
+      <div className="relative z-10 flex min-h-screen flex-col">
+        <Navbar />
+        <main className="flex-1">
+          <Outlet />
+        </main>
+        <FooterSection />
+      </div>
     </div>
   )
 }
