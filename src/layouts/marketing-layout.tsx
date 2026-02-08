5b9c501478c6ab884c2515adc0b433546ee0ae4d
Troxx <121587043+xx0rT@users.noreply.github.com>
2026-02-08 12:05:12 +0100

    Updated marketing-layout.tsx

--- src/layouts/marketing-layout.tsx
+++ src/layouts/marketing-layout.tsx
@@ -19,7 +19,7 @@ export default function MarketingLayout() {
   }, [])
 
   return (
-    <div className="flex min-h-screen flex-col">
+    <div className="flex min-h-screen flex-col overflow-x-clip">
       <Navbar10 />
       <main className="flex-1 pt-20">
         <Outlet />
