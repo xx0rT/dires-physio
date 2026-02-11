6a3b6b794dd9874aa4e473444e208bcdbb8b367e
Troxx <121587043+xx0rT@users.noreply.github.com>
2026-02-11 12:24:25 +0100

    Updated footer.tsx

--- src/components/layout/sections/footer.tsx
+++ src/components/layout/sections/footer.tsx
@@ -246,7 +246,7 @@ const Footer31 = ({ className }: Footer31Props) => {
           >
             <text
               x="50%"
-              y="100%"
+              y="10%"
               textAnchor="middle"
               dominantBaseline="middle"
               fill="currentColor"
